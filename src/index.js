3d86e53e0ebb7897109c1c6c96f046f182e37851
yeonah98 <dbdusdkaa@naver.com>
2022-01-16 18:49:13 +0900

    Feat: "Add title and logo"

--- src/index.js
+++ src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
+
+import './assets/boxicons-2.0.7/css/boxicons.min.css';
+import './assets/css/grid.css';
+import './assets/css/index.css'
+
 import Layout from './components/layout/Layout';
+
+document.title = 'JoYu CRM';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,6 +18,6 @@ ReactDOM.render(
 );
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
+// to log results (for example: reportWe bVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
