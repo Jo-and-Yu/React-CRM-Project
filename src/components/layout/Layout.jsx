b632611aeb01f46725847565b12e985179c97111
okhan1998 <whwlgns111@gmail.com>
2022-01-17 11:07:59 +0900

    feat: "Reset css"

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -8,7 +8,8 @@ const Layout = () => {
     return (
         <Router>
             <div className='layout'>
-                <Sidebar /> // v6 버전에서 렌더 방식이 안되서 이걸로 고쳐놨는데 나중에 다시 수정할 수도 있음 
+            {/* v6 버전에서 렌더 방식이 안되서 이걸로 고쳐놨는데 나중에 다시 수정할 수도 있음  */}
+                <Sidebar /> 
                 <div className='layout__content'>
                     <div className='layout__content-main'>
                         <Routess/>
