3fb40a2287b835cb7ed27e3c6490dc05aab9c5a7
okhan1998 <whwlgns111@gmail.com>
2022-01-19 13:31:04 +0900

    Feat: 'Setup Themes'

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import reportWebVitals from './reportWebVitals';
 
 import './assets/boxicons-2.0.7/css/boxicons.min.css';
 import './assets/css/grid.css';
+import './assets/css/theme.css'
 import './assets/css/index.css'
 
 import Layout from './components/layout/Layout';
